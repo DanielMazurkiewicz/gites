e9d5ad5f340f02748248deb109ed0d376bfa93e0
Daniel Mazurkiewicz <daniel@latanie.info>
2019-06-03 15:28:53 +0200

    basic operations done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ program
     .option('-n, --new [task_title]', 'Starts a new task')
     .option('-s, --save [comment]', 'Saves current work')
     .option('-w, --switch [task_title]', 'Switches to existing task')
+    .option('-f, --from [task_title]', 'Used in conjunction with --new - copies existing task code as base for new task')
     .option('-p, --superpower', 'Allows to save directly to develop or master branch')
     .option('-c, --config [config_name]', 'Switches configuration files (unsupported yet)')
     .parse(process.argv);
@@ -15,8 +16,7 @@ program
 
 const isAwaitingToSave = status => status.not_added.length || status.created.length || status.deleted.length || status.modified.length || status.renamed.length;
 
-simpleGit.fetch((error, info) => console.log(info));
-
+simpleGit.fetch();
 
 
 if (program.save) {
@@ -56,25 +56,68 @@ if (program.switch) {
         }
 
         if (isAwaitingToSave(status)) {
-            console.log('Can not switch to other task due to unsaved work -save it first and then switch')
+            console.log('Can not switch to other task due to unsaved work - save it first and then switch')
             return;
         }
 
+        let info;
+        simpleGit.branch((error, i) => info = i);
+        if (!info.branches[program.switch]) {
+            console.log('Task doesn\'t exist')
+            return;
+        }
+        
 
         if (program.switch === true) {
             console.log('Missing task title');
         } else {
-            simpleGit.add('.');
-            simpleGit.commit(program.save);
-            simpleGit.push();
+            simpleGit.checkout(program.switch);
+            simpleGit.pull();
         }
-
     });
-
 }
 
-if (program.new) {
 
+if (program.new) {
+    simpleGit.status((error, status) => {
+        if (status.conflicted.length) {
+            console.log('Can not create new task due to existing conflicts: ' + status.conflicted.join(', '))
+            return;
+        }
+
+        if (isAwaitingToSave(status)) {
+            console.log('Can not create new task due to unsaved work - save it first')
+            return;
+        }
+
+        let info;
+        simpleGit.branch((error, i) => info = i);
+        if (info.branches[program.new]) {
+            console.log('Task already exist, use --switch option if you want to switch to that task')
+            return;
+        }
+        
+        const defaultSourceBranch = info.branches.develop ? 'develop' : info.branches.master ? 'master' : false;
+
+        if (!defaultSourceBranch && !program.from) {
+            console.log('Missing branch master and/or develop');
+            return;
+        }
+
+        if (program.from === true) {
+            console.log('Missing task title of base code');
+            return;
+        }
+
+        if (program.new === true) {
+            console.log('Missing task title');
+        } else {
+            simpleGit.checkout(program.from || defaultSourceBranch);
+            simpleGit.pull();
+
+            simpleGit.checkout(program.new);
+        }
+    });
 }
 
 
